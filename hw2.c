ec2a291864d09a050e77ea26fe5fe88df161ae10
Aditya-1506 <adityaraja2006@gmail.com>
2024-05-18 15:05:52 +0400

    Add files via upload

---
+++ hw2.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+ void main ()
+
+ {   
+     int a=25;
+     int b=4;
+     int c=7;
+     int d=3;
+     int e=6;
+     float f=((a*b)-c+e)/d;
+     printf("f:the product is %f",f);
+ }
+ 
